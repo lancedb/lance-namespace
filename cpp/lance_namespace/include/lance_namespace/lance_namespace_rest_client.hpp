4f74d31a20290068935259c2f1768ec5910974cd
Ally Heev <allyheev@gmail.com>
2026-06-21 22:11:15 +0530

    re-export rest_client headers

--- cpp/lance_namespace/include/lance_namespace/lance_namespace_rest_client.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#if defined(__has_include)
-#if __has_include(<lance_namespace_rest_client/lance_namespace.hpp>)
-#include <lance_namespace_rest_client/lance_namespace.hpp>
-#endif
-#endif
